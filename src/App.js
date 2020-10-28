a0671c1e78417ee9efb308c59eedaf07e776a0fe
jremy813 <jremywatkins@gmail.com>
2020-10-27 20:00:42 -0400

    created and mapped over data

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header.js'
 import Hero from './components/Hero'
+import Recommended from './components/Recommended'
 
 function App() {
   return (
     <div className="app">
       <Header />
       <Hero />
-    
+      <Recommended />
        </div>
   );
 }
---
+++ src/components/Recommended.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import './Recommended.scss'
+import Image1 from '../assets/AdobeStock_236655481.png'
+import Image2 from '../assets/AdobeStock_118120200.png'
+import Image3 from '../assets/AdobeStock_136908398.png'
+import Image4 from '../assets/AdobeStock_173519034.png'
+
+function Recommended() {
+
+        const data = [
+            {
+            image:`url(${Image1})`,
+            name: 'T-Shirt Summer Vibes',
+            price:'$89.99'
+            },
+            {
+                image:`url(${Image2})`,
+                name: 'Loose Knit 3/5 Sleeve',
+                price:'$119.99'
+            },
+            {
+                image:`url(${Image3})`,
+                name:'Basic Slim Fit T-Shirt',
+                price:'$79.99',
+            },
+            {
+                image:`url(${Image4})`,
+                name:'Loose Textured T-Shirt',
+                price:'$199.99'
+            }
+        ]
+    return (
+        <div className='recommended'>
+          <div className='recommended__Title'>
+              <h3>Selected just for  you</h3>
+              <span>Show more</span>
+          </div>
+          
+          <div className='recommended__Listing'>
+              
+             {data.map(data => (
+                 <>
+                  <div className='recommended__Image' style={{backgroundImage: data.image}}>
+                  
+                  </div>
+                  <div className='recommended__Item'>
+                      <span>{data.name}</span>
+                      <span>{data.price}</span>
+                  </div>
+                  </>
+             ))}
+          </div>
+            
+        </div>
+    )
+}
+
+export default Recommended
